0f58381c942b230898281e0463a7adb673905b6e
idcargill <Idcargill@gmail.com>
2021-06-06 11:48:04 -0700

    Canvas / Webpack built

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import '../style/main.css';
 import $ from 'jquery';
 
-const test = $('h1')[0];
+const root = $('#root');
+root[0].textContent = 'Jquery fish';
 
-test.textContent = 'hello with Jquery';
+const sketch = document.querySelector('#sketch'); // canvas obj
+const ctx = sketch.getContext('2d');
+
+const mouseLocation = (ev) => {
+  const location = [ev.x, ev.y];
+  console.log(location);
+  ctx.fillStyle = 'green';
+  ctx.fillRect(10, 10, 50, 50);
+
+  ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+  ctx.fillRect(30, 30, 50, 50);
+};
+
+// sketch.addEventListener('mousedown', mouseLocation);
+
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 50, 50);
+
+ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+ctx.fillRect(30, 30, 50, 50);
