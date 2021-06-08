6db72d1e6d7efce2a89da8ac111f0c1f13eaecf5
Ian Cargill <ianc@zillowgroup.com>
2021-06-08 09:14:52 -0700

    start script = server

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const ctx = sketch.getContext('2d');
 // == EXAMPLES ========================
 // help.bubble(ctx, 200, 200, 20);
 // help.bubble(ctx, 100, 200, 5);
-// smirkFace(ctx);
+smirkFace(ctx);
 // ex.pacMan();
 // help.triangle([100, 100], [50, 50], [150, 50]);
 // help.drawLine([100, 100], [205, 210], 3);
@@ -27,32 +27,32 @@ const ctx = sketch.getContext('2d');
 
 var points = [];
 function polyLines() {
-  ctx.beginPath();
-  ctx.moveTo(points[0][0], points[0][1]);
-  points.forEach((i) => {
-    ctx.lineTo(i[0], i[1]);
-    ctx.stroke();
-  });
-  console.log(points);
+	ctx.beginPath();
+	ctx.moveTo(points[0][0], points[0][1]);
+	points.forEach((i) => {
+		ctx.lineTo(i[0], i[1]);
+		ctx.stroke();
+	});
+	console.log(points);
 }
 
 function createPoly(ev) {
-  const box = sketch.getBoundingClientRect();
-  const cX = ev.clientX - box.left;
-  const cY = ev.clientY - box.top;
-  ctx.fillRect(cX, cY, 5, 5);
-  ctx.stroke();
-  points.push([cX, cY]);
-  // console.log(points);
-  polyLines();
+	const box = sketch.getBoundingClientRect();
+	const cX = ev.clientX - box.left;
+	const cY = ev.clientY - box.top;
+	ctx.fillRect(cX, cY, 5, 5);
+	ctx.stroke();
+	points.push([cX, cY]);
+	// console.log(points);
+	polyLines();
 }
 
 // Clear Button
 function clearAll() {
-  points = [];
-  ctx.clearRect(0, 0, 400, 400);
-  sketch.removeEventListener('mousedown', createPoly);
-  polyBtn.removeEventListener('mousedown', createPoly);
+	points = [];
+	ctx.clearRect(0, 0, 400, 400);
+	sketch.removeEventListener('mousedown', createPoly);
+	polyBtn.removeEventListener('mousedown', createPoly);
 }
 const btn = document.querySelector('#clear');
 btn.addEventListener('click', clearAll);
