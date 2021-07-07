7aa8988ff8650bfd4563f9d06d35541acb4ea690
Ian Cargill <ianc@zillowgroup.com>
2021-07-06 19:52:33 -0700

    webpack html template - docs folder for server

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
 	mode: 'development',
 	output: {
 		filename: 'main.js',
-		path: path.resolve(__dirname, 'docs/dist'),
+		path: path.resolve(__dirname, 'docs'),
 	},
 	// watch: true,
 	devServer: {
 		hot: true,
-		contentBase: path.join(__dirname, 'docs/dist'),
+		contentBase: path.join(__dirname, 'docs'),
 		compress: true,
 		port: 9000,
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+		}),
+	], //
 	module: {
 		rules: [
 			{
 				test: /\.css$/i,
 				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.html$/i,
+				use: 'html-loader',
 			},
 			{
 				test: /\.m?js$/,
